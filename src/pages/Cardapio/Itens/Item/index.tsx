dca9d8a542b5a844ad76018e893cbcbfc58dd4c3
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-11-08 15:01:21 -0300

    Adicionado as imagens e cores das categorias

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -2,21 +2,36 @@ import React from "react";
 import styles from "./Item.module.scss";
 
 import logo from "assets/logo.svg";
+import cardapio from "../itens.json";
 
-export default function Item() {
+import classnames from "classnames";
+
+type ItemProps = typeof cardapio[0];
+
+export default function Item(props: ItemProps) {
   return (
     <div className={styles.item}>
-      <img className={styles.item__imagem} src={logo} alt="Imagem do Item" />
+      <img className={styles.item__imagem} src={props.photo} alt={props.title} />
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
-          <h2>Macarrão</h2>
-          <p>Descrição do Macarrão</p>
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>Massa</div>
-          <div className={styles.item__porcao}>400g</div>
-          <div className={styles.item__qtdpessoas}>Server 2 pessoas</div>
-          <div className={styles.item__valor}>R$50</div>
+          <div
+            className={classnames({
+              [styles.item__tipo]: true,
+              [styles[`item__tipo__${props.category.label.toLowerCase()}`]]:
+                true,
+            })}
+          >
+            {props.category.label}
+          </div>
+          <div className={styles.item__porcao}>{props.size}g</div>
+          <div className={styles.item__qtdpessoas}>
+            Serve {props.serving} pessoa{props.serving === 1 ? "" : "s"}
+          </div>
+          <div className={styles.item__valor}>R$ {props.price.toFixed(2)}</div>
         </div>
       </div>
     </div>
