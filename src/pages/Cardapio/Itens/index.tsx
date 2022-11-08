9da87de6d8ad2b9f8b28f92c9f91531b98c19836
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-11-08 16:04:29 -0300

    Add filtros

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Item from "./Item";
 import styles from "./Itens.module.scss";
 
 import cardapio from "./itens.json";
 
-export default function Itens() {
+interface ItensProps {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
+}
+
+export default function Itens(props: ItensProps) {
+  const [lista, setLista] = useState(cardapio);
+  const { busca, filtro } = props;
+
+  function testeBusca(title: string) {
+    const regex = new RegExp(busca, "i");
+    return regex.test(title);
+  }
+
+  function testeFiltro(id: number) {
+    if (filtro !== null) return filtro === id;
+    return true
+  }
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(
+      (item) => testeBusca(item.title) && testeFiltro(item.category.id)
+    );
+    setLista(novaLista);
+  }, [busca, filtro]);
+
   return (
     <div className={styles.itens}>
-      {cardapio.map((item) => (
+      {lista.map((item) => (
         <Item key={item.id} {...item} />
       ))}
     </div>
